0121ceae3d9d8392e671508fb81f9ddeac36364c
Hitow22 <Hitow22@gmail.com>
2024-12-21 21:34:55 +0300

    final games

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Random random = new Random();
-        int originalNumber = random.nextInt(100) + 1;
+        int originalNumber = random.nextInt(1,101);
         Scanner input = new Scanner(System.in);
-        System.out.print("Введите число : ");
-        int guessNumber = input.nextInt();
-        while (guessNumber != originalNumber) {
-            if (guessNumber > originalNumber) {
-                System.out.println(guessNumber + " больше того, что загадал компьютер" +
-                        "\nВведите число : ");
-            } else {
-                System.out.println(guessNumber + " меньше того, что загадал компьютер" +
-                        "\nВведите число : ");
-            }
+        int guessNumber;
+        do {
+            System.out.print("Введите число : ");
             guessNumber = input.nextInt();
-        }
+
+            if (guessNumber > originalNumber) {
+                System.out.println(guessNumber + " больше того, что загадал компьютер.");
+            } else if (guessNumber < originalNumber) {
+                System.out.println(guessNumber + " меньше того, что загадал компьютер.");
+            }
+        } while (guessNumber != originalNumber);
+
         System.out.println("Вы победили!");
     }
 }
