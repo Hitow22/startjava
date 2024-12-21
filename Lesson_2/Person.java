7729fb2a61f65bfbe511d36c6fda7d592352e029
Hitow22 <Hitow22@gmail.com>
2024-12-21 21:33:13 +0300

    first person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    
+    String sex = "мужской";
+    String name = "Андрей";
+    float height = 1.7f;
+    float weight = 67.5f;
+    int age = 25;
+
+    void walk() {
+        System.out.println("Андрей может ходить");
+    }
+
+    void seat() {
+        System.out.println("Андрей может сидеть");
+    }
+
+    void run() {
+        System.out.println("Андрей может бегать");
+    }
+
+    void speak() {
+        System.out.println("Андрей может говорить");
+    }
+
+    void learnJava() {
+        System.out.println("Андрей может учить Java");
+    }
+}
+
